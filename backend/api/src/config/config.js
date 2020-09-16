2e4374969eb3b7606bb5dc44716b4be9a1323cef
shagunchaurasia <chaurasia.shagun@gmail.com>
2020-09-16 13:13:20 +0900

    Modified package json

--- backend/api/src/config/config.js
+++ backend/api/src/config/config.js
@@ -1,4 +1,5 @@
 const dotenv = require("dotenv").config();
+console.log(process.env);
 module.exports = {
   ENV: process.env.ENV,
   PORT: process.env[process.env.ENV + "_PORT"],
--- backend/api/src/index.js
+++ backend/api/src/index.js
@@ -9,9 +9,9 @@ const swaggerDoc = yaml.load("./src/swagger/swagger.yaml");
 const schemeRoutes = require("./routes/schemes");
 const schemeTypeRoutes = require("./routes/schemeTypes");
 const authRoutes = require("./routes/authRoutes");
-const stateRoutes = require('./routes/stateRoutes');
-const cityRoutes = require('./routes/cityRoutes');
-const userTypeRoutes = require('./routes/userTypeRoutes');
+const stateRoutes = require("./routes/stateRoutes");
+const cityRoutes = require("./routes/cityRoutes");
+const userTypeRoutes = require("./routes/userTypeRoutes");
 const mongoDBConnection = require("./config/database");
 const colors = require("colors");
 const errorHandler = require("./middleware/error");
@@ -32,10 +32,10 @@ app.use(bodyParser.json({ extended: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/schemes", schemeRoutes);
-app.use('/api/schemeTypes',schemeTypeRoutes);
-app.use('/api/state',stateRoutes);
-app.use('/api/city',cityRoutes);
-app.use('/api/userType',userTypeRoutes)
+app.use("/api/schemeTypes", schemeTypeRoutes);
+app.use("/api/state", stateRoutes);
+app.use("/api/city", cityRoutes);
+app.use("/api/userType", userTypeRoutes);
 app.use(errorHandler);
 
 // app.use("", (request, response, next) => {
